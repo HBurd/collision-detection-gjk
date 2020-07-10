41e87c74e2f4627733b299172300f26ca1e99407
HBurd <hugoburd@gmail.com>
2020-07-10 11:23:01 -0700

    Added function definition for intersect_gjk
    This is to be the main function of this project. Its purpose is to determine if two 3d convex shapes are intersecting.

--- app/demo.cpp
+++ app/demo.cpp
@@ -7,6 +7,7 @@ using namespace demo::math;
 
 struct CubeData
 {
+    Vec3 position;
     Mat3 orientation;
 };
 
@@ -14,24 +15,35 @@ Vec3 cube_support(Vec3 dir, const CubeData& data)
 {
     // These are just points on a convex hull
     std::array<Vec3, 8> vertices = {
-        data.orientation * Vec3(0.5f, 0.5f, 0.5f),
-        data.orientation * Vec3(0.5f, 0.5f, -0.5f),
-        data.orientation * Vec3(0.5f, -0.5f, 0.5f),
-        data.orientation * Vec3(0.5f, -0.5f, -0.5f),
-        data.orientation * Vec3(-0.5f, 0.5f, 0.5f),
-        data.orientation * Vec3(-0.5f, 0.5f, -0.5f),
-        data.orientation * Vec3(-0.5f, -0.5f, 0.5f),
-        data.orientation * Vec3(-0.5f, -0.5f, -0.5f),
+        data.position + data.orientation * Vec3(0.5f, 0.5f, 0.5f),
+        data.position + data.orientation * Vec3(0.5f, 0.5f, -0.5f),
+        data.position + data.orientation * Vec3(0.5f, -0.5f, 0.5f),
+        data.position + data.orientation * Vec3(0.5f, -0.5f, -0.5f),
+        data.position + data.orientation * Vec3(-0.5f, 0.5f, 0.5f),
+        data.position + data.orientation * Vec3(-0.5f, 0.5f, -0.5f),
+        data.position + data.orientation * Vec3(-0.5f, -0.5f, 0.5f),
+        data.position + data.orientation * Vec3(-0.5f, -0.5f, -0.5f),
     };
+
+    float max_dot = 0.0f;
+    Vec3 max_dot_v = Vec3(0.0f, 0.0f, 0.0f);
 
     for (auto v : vertices)
     {
-
+        max_dot = std::max(max_dot, dot(dir, v));
+        max_dot_v = v;
     }
+
+    return max_dot_v;
 }
 
 int main()
 {
+    CubeData data1;
+    CubeData data2;
+    data2.position = Vec3(1.0f, 1.0f, 1.0f);
+
+    geometry::intersect_gjk(cube_support, data1, cube_support, data2);
 
     return 0;
 }
--- include/gjk.hpp
+++ include/gjk.hpp
@@ -4,6 +4,15 @@
 namespace geometry
 {
 
+template<class Vec3, class Data>
+using SupportFn = Vec3 (*) (Vec3 dir, const Data& data);
+
+template<class Vec3, class Data1, class Data2, class Real = typename Vec3::real_type>
+bool intersect_gjk(SupportFn<Vec3, Data1> support1, const Data1& data1, SupportFn<Vec3, Data2> support2, const Data2& data2)
+{
+
+}
+
 }
 
 #endif
