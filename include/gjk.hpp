5b2d5f399d7cfa6f7d9e2b59259d62fa53bebb2f
HBurd <hugoburd@gmail.com>
2020-07-20 17:07:28 -0700

    Implemented the function for finding direction normal to 4-simplex pointing to origin.
    This is a complicated function with many cases, and it is possible that
    some of them are not actually possible to reach, based on how this
    function is called in GJK.
    
    I have not tested this function yet. To save time, I am moving first to
    end-to-end tests of GJK intersection tests, rather than testing the
    internal components.

--- include/gjk.hpp
+++ include/gjk.hpp
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <cassert>
+#include <algorithm>
 
 namespace geometry
 {
@@ -108,6 +109,10 @@ namespace geometry
             {
                 // Origin is above the triangle
                 d = triangle_normal;
+
+                // Make the search direction opposite the triangle normal,
+                // so the 4-simplex is always consistently constructed.
+                std::swap(simplex[1], simplex[2]);
             }
             else
             {
@@ -120,10 +125,138 @@ namespace geometry
     template <class Vec3>
     bool simplex4_dir(Vec3* simplex, Vec3& d)
     {
+        using Real = decltype(Vec3::x);
 
+        /* Triangle 0 vertices are 0, 1, 2.
+           Triangle 1 vertices are 1, 0, 3.
+           Triangle 2 vertices are 2, 3, 0.
+           Triangle 3 vertices are 3, 2, 1.
+           All triangles are counter-clockwise when looking from the outside.
+           All normals are pointing outward.
+
+           We know beforehand that the origin is on the negative side of triangle 0,
+           so it does not need to be tested. The origin is also not closest to vertex
+           3, so it need not be tested. There may be several other cases that
+           can be eliminated, but I have not figured out which ones yet.
+        */
+
+        //Vec3 triangle0_normal = cross(simplex[1] - simplex[0], simplex[2] - simplex[0]);
+        Vec3 triangle1_normal = cross(simplex[0] - simplex[1], simplex[3] - simplex[1]);
+        Vec3 triangle2_normal = cross(simplex[3] - simplex[2], simplex[0] - simplex[2]);
+        Vec3 triangle3_normal = cross(simplex[2] - simplex[3], simplex[1] - simplex[3]);
+
+        // Pointing toward face, away from edge
+        Vec3 triangle1_edge12_boundary_normal = cross(triangle1_normal, simplex[3] - simplex[0]);
+        Vec3 triangle1_edge13_boundary_normal = cross(triangle1_normal, simplex[1] - simplex[3]);
+        Vec3 triangle2_edge21_boundary_normal = cross(triangle2_normal, simplex[0] - simplex[3]);
+        Vec3 triangle2_edge23_boundary_normal = cross(triangle2_normal, simplex[3] - simplex[2]);
+        Vec3 triangle3_edge31_boundary_normal = cross(triangle3_normal, simplex[3] - simplex[1]);
+        Vec3 triangle3_edge32_boundary_normal = cross(triangle3_normal, simplex[2] - simplex[3]);
+
+        // If positive, the origin is on the negative side of the plane
+        Real side_of_triangle1 = dot(triangle1_normal, simplex[0]);
+        Real side_of_triangle2 = dot(triangle2_normal, simplex[3]);
+        Real side_of_triangle3 = dot(triangle3_normal, simplex[2]);
+
+        Real  side_of_triangle1_edge12_boundary = dot(triangle1_edge12_boundary_normal, simplex[0]);
+        Real  side_of_triangle1_edge13_boundary = dot(triangle1_edge13_boundary_normal, simplex[3]);
+        Real  side_of_triangle2_edge21_boundary = dot(triangle2_edge21_boundary_normal, simplex[3]);
+        Real  side_of_triangle2_edge23_boundary = dot(triangle2_edge23_boundary_normal, simplex[2]);
+        Real  side_of_triangle3_edge31_boundary = dot(triangle3_edge31_boundary_normal, simplex[1]);
+        Real  side_of_triangle3_edge32_boundary = dot(triangle3_edge32_boundary_normal, simplex[3]);
+
+        if (side_of_triangle1 < Real(0))
+        {
+            if (side_of_triangle1_edge12_boundary >= Real(0))
+            {
+                // Closest to edge12 or triangle2
+                if (side_of_triangle2_edge21_boundary < 0)
+                {
+                    // Closest to triangle2
+                    d = triangle2_normal;
+                }
+                else
+                {
+                    // Closest to edge12
+                    d = cross(cross(simplex[3] - simplex[0], -simplex[0]), simplex[3] - simplex[0]);
+                }
+            }
+            else if (side_of_triangle1_edge13_boundary >= Real(0))
+            {
+                // Closest to edge13 or triangle3
+                if (side_of_triangle2_edge23_boundary < 0)
+                {
+                    // Closest to triangle3
+                    d = triangle3_normal;
+                }
+                else
+                {
+                    // Closest to edge13
+                    d = cross(cross(simplex[1] - simplex[3], -simplex[3]), simplex[1] - simplex[3]);
+                }
+            }
+            else
+            {
+                // Closest to triangle1
+                d = triangle1_normal;
+            }
+        }
+        else if (side_of_triangle2 < Real(0))
+        {
+            if (side_of_triangle2_edge23_boundary >= Real(0))
+            {
+                // Closest to edge23 or triangle3
+                if (side_of_triangle3_edge32_boundary < Real(0))
+                {
+                    // Closest to triangle3
+                    d = triangle3_normal;
+                }
+                else
+                {
+                    // Closest to edge23
+                    d = cross(cross(simplex[3] - simplex[2], -simplex[2]), simplex[3] - simplex[2]);
+                }
+            }
+            else if (side_of_triangle2_edge21_boundary >= Real(0))
+            {
+                // Closest to edge12, since closest to triangle1 case is already ruled out
+                d = cross(cross(simplex[3] - simplex[0], -simplex[0]), simplex[3] - simplex[0]);
+            }
+            else
+            {
+                // Closest to triangle2
+                d = triangle2_normal;
+            }
+        }
+        else if (side_of_triangle3 < Real(0))
+        {
+            if (side_of_triangle3_edge32_boundary >= Real(0))
+            {
+                // Closest to edge32, since closest to triangle2 case already ruled out
+                d = cross(cross(simplex[3] - simplex[2], -simplex[2]), simplex[3] - simplex[2]);
+            }
+            else if (side_of_triangle3_edge31_boundary >= Real(0))
+            {
+                // Closest to edge31, since closest to triangle2 case already ruled out
+                d = cross(cross(simplex[1] - simplex[3], -simplex[3]), simplex[1] - simplex[3]);
+            }
+            else
+            {
+                // Closest to triangle3
+                d = triangle3_normal;
+            }
+        }
+        else
+        {
+            // Origin is contained in the triangle
+            // TODO: Should d be set here?
+            return true;
+        }
+
+        return false;
     }
 
-    // We are treating resting contact as an intersection
+    // TODO: are we treating resting contact as an intersection?
     template <class Vec3, class Data1, class Data2>
     bool intersect_gjk(SupportFn<Vec3, Data1> support1, const Data1& data1, SupportFn<Vec3, Data2> support2, const Data2& data2)
     {
