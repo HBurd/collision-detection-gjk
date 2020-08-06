b2cc1d6e1aeb368f309b9dca459fad165e1c74e1
HBurd <hugoburd@gmail.com>
2020-08-05 22:00:42 -0700

    Fixed simplex naming convention in test file

--- app/test_gjk.cpp
+++ app/test_gjk.cpp
@@ -18,43 +18,43 @@ void assert_equal(Vec3 v1, Vec3 v2)
     assert(abs(v1.x - v2.x) < epsilon && abs(v1.y - v2.y) < epsilon && abs(v1.z - v2.z) < epsilon);
 }
 
-void test_simplex1_dir()
+void test_simplex0_dir()
 {
     Vec3 simplex(1.0f, 2.0f, 1.0f);
     Vec3 d;
-    geometry::simplex1_dir(&simplex, d);
+    geometry::simplex0_dir(&simplex, d);
     assert_equal(d, Vec3(-1.0f, -2.0f, -1.0f));
 }
 
-void test_simplex2_dir()
+void test_simplex1_dir()
 {
-    Vec3 simplex1[2] = {
+    Vec3 simplex0[2] = {
         Vec3(0.0f, -1.0f, 0.0f),
         Vec3(0.1f, 1.0f, 0.0f)
     };
     Vec3 expected1(-2.0f, 0.1f, 0.0f);
     Vec3 d;
 
-    geometry::simplex2_dir(simplex1, d);
+    geometry::simplex1_dir(simplex0, d);
     
     assert_equal(dot(d, expected1), d.mag()*expected1.mag());
 
-    Vec3 simplex2[2] = {
+    Vec3 simplex1[2] = {
         Vec3(0.0f, -1.0f, 0.0f),
         Vec3(-0.1f, 1.0f, 0.0f)
     };
     Vec3 expected2(2.0f, 0.1f, 0.0f);
 
-    geometry::simplex2_dir(simplex2, d);
+    geometry::simplex1_dir(simplex1, d);
 
     assert_equal(dot(d, expected2), d.mag()*expected2.mag());
 }
 
-void test_simplex3_dir()
+void test_simplex2_dir()
 {
     std::size_t simplex_size = 3;
 
-    Vec3 simplex1[3] = {
+    Vec3 simplex0[3] = {
         Vec3(-1.0f, 0.0f, -1.0f),
         Vec3(1.0f, -1.0f, -1.0f),
         Vec3(0.0f, 1.0f, -1.0f),
@@ -62,25 +62,25 @@ void test_simplex3_dir()
     Vec3 expected1(0.0f, 0.0f, 1.0f);
     Vec3 d;
 
-    geometry::simplex3_dir(simplex1, simplex_size, d);
+    geometry::simplex2_dir(simplex0, simplex_size, d);
     assert(simplex_size == 3);
 
     assert_equal(dot(d, expected1), d.mag()*expected1.mag());
 
-    Vec3 simplex2[3] = {
+    Vec3 simplex1[3] = {
         Vec3(0.0f, 1.0f, -1.0f),
         Vec3(1.0f, 0.0f, -1.0f),
         Vec3(1.0f, 1.0f, -1.0f),
     };
     Vec3 expected2(-0.5f, -0.5f, 1.0f);
 
-    geometry::simplex3_dir(simplex2, simplex_size, d);
+    geometry::simplex2_dir(simplex1, simplex_size, d);
     assert(simplex_size == 2);
 
     assert(dot(d, expected2) - d.mag()*expected2.mag() < 0.0001f);
 }
 
-void test_simplex4_dir()
+void test_simplex3_dir()
 {
 
 }
@@ -88,10 +88,10 @@ void test_simplex4_dir()
 // This tests functions that aren't part of the interface.
 void test_gjk_internals()
 {
+    test_simplex0_dir();
     test_simplex1_dir();
     test_simplex2_dir();
     test_simplex3_dir();
-    test_simplex4_dir();
 }
 
 
